32308bab627c4cf01c72d3fa735801f3ca28ea8d
annahico <annahico@gmail.com>
2024-05-22 20:23:52 +0200

    feat: create folder routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const sequelize = require("./database/db");
-const { Author} =
+const apiRoutes = require("./routes");
+
 dotenv.config();
 
 const app = express();
@@ -17,57 +18,19 @@ app.get("/api/healthy", (req, res) => {
    });
 });
 
-// CRUD DE AUTHORS
+// Register API routes
+app.use("/api", apiRoutes);
 
-// create
-app.post("/api/authors", async (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: "Author created successfully",
+sequelize
+   .authenticate()
+   .then(() => {
+      console.log("🛢️  Database authenticated");
+
+      // start the server
+      app.listen(PORT, () => {
+         console.log(`🚀 Server listening on port: ${PORT}`);
+      });
+   })
+   .catch(() => {
+      console.error("Error authenticating database");
    });
-});
-
-// get all
-app.get("/api/authors", async (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: "Authors retreived successfully",
-   });
-});
-
-// get by id
-app.get("/api/authors/:id", async (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: "Author retreived successfully",
-   });
-});
-
-// update
-app.put("/api/authors/:id", async (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: "Author updated successfully",
-   });
-});
-
-// delete
-app.delete("/api/authors/:id", async (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: "Author deleted successfully",
-   });
-});
-
-// CRUD DE BOOKS
-
-// server
-app.listen(PORT, () => {
-   console.log( `Server listening on port: ${PORT}`);
-});
-
-sequelize.autheticate().then(() => {
-   console.log('Database authenticated');
-}).catch (() => {
-   console.log('Error authenticating database');
-});
