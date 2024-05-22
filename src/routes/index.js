90760442724769d24ecf51b2bb0cc54c2b83c185
annahico <annahico@gmail.com>
2024-05-22 20:25:34 +0200

    feat: fixed author  routes

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authorRoutes = require("./author.routes");
-const bookRoutes = require("./book.routes");
-
-router.use("/authors", authorRoutes);
-router.use("/books", bookRoutes);
-
-module.exports = router;
