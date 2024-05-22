07f3fb1bcb464c02cee38f15d857f718f6231d22
annahico <annahico@gmail.com>
2024-05-22 19:41:35 +0200

    feat: book and author controller

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,34 +1,128 @@
-const authorController = {}
+const authorController = {};
+const { Author, Book } = require("../models");
 
-authorController.createAuthor =  (req, res) => {
-  // REcuperamos la info
-	console.log(req.body);
-	const name = req.body.name;
-	const nationality = req.body.nationality;
-	console.log(name);
-	console.log(nationality);
+authorController.create = async (req, res) => {
+  const { name, nationality } = req.body;
 
-  // Validar Info. Esto son solo ejemplos
-  if(!name) {
-    return res.status(400).json({
+  try {
+    if (!name || !nationality) {
+      res.status(400).json({
+        success: true,
+        message: "Invalid name or nationality",
+      });
+      return;
+    }
+
+    await Author.create({
+      name,
+      nationality,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Author created successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
       success: false,
-      message: "Name required",
+      message: "Error creating author",
+      error: error.message,
     });
   }
+};
 
-  // Si hace falta tratamos la info, esto son solo ejemplos
-  let newNationality = nationality.toLowerCase()
-  console.log(newNationality);
+authorController.getAll = async (req, res) => {
+  try {
+    const authors = await Author.findAll();
 
-  // Atacar a la bd para crear el nuevo registro del author
-  
+    res.status(200).json({
+      success: true,
+      message: "Authors retreived successfully",
+      data: authors,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error retreiving authors",
+      error: error.message,
+    });
+  }
+};
 
-	res.status(201).json({
-		success: true,
-		message: "Author created",
-	});
-}
+authorController.getById = async (req, res) => {
+  const authorId = req.params.id;
 
+  try {
+    const author = await Author.findByPk(authorId, {
+      include: [
+        {
+          model: Book,
+          as: "books",
+          attributes: { exclude: ["createdAt", "updatedAt", "author_id"] },
+        },
+      ],
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
 
+    if (!author) {
+      res.status(404).json({
+        success: true,
+        message: "Author not found",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Author retreived successfully",
+      data: author,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error retreiving author",
+      error: error.message,
+    });
+  }
+};
+
+authorController.update = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+    const authorData = req.body;
+
+    await Author.update(authorData, {
+      where: {
+        id: authorId,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Author updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error updating author",
+      error: error.message,
+    });
+  }
+};
+
+authorController.delete = async (req, res) => {
+  const authorId = req.params.id;
+
+  await Author.destroy({
+    where: {
+      id: authorId,
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Author deleted successfully",
+  });
+};
 
 module.exports = authorController;
